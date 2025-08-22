234969088bab37c2886729e7a86c0a0ba465bce9
Michael Walter <michael.walter@eps.ch>
2025-08-22 22:48:35 +0200

    added lazy loading

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -1,20 +1,29 @@
-import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { NgModule, isDevMode, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { App } from './app';
 
+// NgRx
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
 @NgModule({
-  declarations: [
-    App
-  ],
+  declarations: [App],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({}),                // root reducers
+    EffectsModule.forRoot([]),              // root effects
+    StoreRouterConnectingModule.forRoot(),  // router <-> store
+    StoreDevtoolsModule.instrument({        // Redux DevTools
+      maxAge: 25,
+      logOnly: !isDevMode()
+    }),
   ],
-  providers: [
-    provideBrowserGlobalErrorListeners()
-  ],
+  providers: [provideBrowserGlobalErrorListeners()],
   bootstrap: [App]
 })
-export class AppModule { }
+export class AppModule {}
