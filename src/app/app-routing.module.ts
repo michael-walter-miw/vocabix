48342beed068b47871d4cad0e3764245220a94f5
Michael Walter <michael.walter@eps.ch>
2025-08-23 19:15:11 +0200

    first landing page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  { path: '', loadComponent: () => import('./features/home/home').then(m => m.Home) },
   { path: 'edit', loadComponent: () => import('./features/edit/edit').then(m => m.Edit) },
   { path: 'train', loadComponent: () => import('./features/train/train').then(m => m.Train) },
-  { path: 'exam', loadComponent: () => import('./features/exam/exam').then(m => m.Exam) }
+  { path: 'exam', loadComponent: () => import('./features/exam/exam').then(m => m.Exam) },
+  { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
